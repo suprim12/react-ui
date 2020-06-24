4f9a9ac1342a1d87c0105f2d835350149115af43
suprim12 <suprimofficial@yahoo.com>
2020-06-25 01:14:21 +0600

    Final Setup Push

---
+++ examples/react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text} from 'react-ui'
+
+const App = () => {
+    return (
+        <React.Fragment>
+        <Text>Hi Text Component</Text>
+        </React.Fragment>
+    )
+}
+
+export default App
